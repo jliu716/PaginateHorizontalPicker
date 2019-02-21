55f0b96440a2c6376acd1c0f0029dea4979c0993
Jiayi Liu <liujiayi19920724@gmail.com>
2019-02-21 20:15:41 +1300

    Initial Commit

---
+++ PaginateHorizontalPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaginateHorizontalPicker
+//
+//  Created by Beethoven on 21/02/19.
+//  Copyright © 2019 Jiayi Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
